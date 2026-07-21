8fbc71fe9d61feee998098be4165c4fa3d8018dd
wuqiwei <wuqiwei4@huawei.com>
2026-07-21 11:24:57 +0000

    [RL]sglang-patch

--- python/sglang/srt/utils/common.py
+++ python/sglang/srt/utils/common.py
@@ -170,13 +170,13 @@ def register_xpu_device_properties_for_dynamo() -> None:
     if xpu_props_type is not None:
         dynamo_utils.common_constant_types.add(xpu_props_type)
 
-
+from relax.utils.device import is_npu_available
 @lru_cache(maxsize=1)
 def is_npu() -> bool:
     if not hasattr(torch, "npu"):
         return False
 
-    if not torch.npu.is_available():
+    if not is_npu_available:
         raise RuntimeError(
             "torch_npu detected, but NPU device is not available or visible."
         )
